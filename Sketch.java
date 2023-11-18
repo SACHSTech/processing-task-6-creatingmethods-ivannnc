049a2b5c077aa76c01b48d2962dfa63797b9a986
ivannnc <junxian.chen24@ycdsbk12.ca>
2023-11-18 13:14:13 -0500

    submit

--- Sketch.java
+++ Sketch.java
@@ -23,14 +23,45 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  // declare variables
+  float floatX;
+  float floatY;
+  float floatSize = 20;
+  int intFaceColour = 0;
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // draw for loop to print out spider
+  for(floatX = 40; floatX < 400; floatX += 40) {
+    for(floatY = 40; floatY < 400; floatY +=400) {
+      intFaceColour += 3;
+      floatSize += 10;
+      drawFace(floatX, floatY, floatSize, intFaceColour);
+      fill(250, 20, 90);
+    }
   }
+
+  /**
+   * @param floatFaceX
+   * @param floatFaceY
+   * @param floatFaceSize
+   * @param intFaceColour
+   */
   
-  // define other methods down here.
+  }
+
+  private void drawFace(float floatFaceX, float floatFaceY, float floatFaceSize, int intFaceColour) {
+
+    // head
+    fill(51, 153, 255);
+    ellipse(floatFaceX, floatFaceY, floatFaceSize, (float) (floatFaceSize / 1.5));
+
+    // eyes
+    fill(255, 255, 255);
+    ellipse(floatFaceX - floatFaceSize / 5, floatFaceY - floatFaceY / 30, floatFaceSize / 10, floatFaceSize / 10);
+    ellipse(floatFaceX + floatFaceSize / 10, floatFaceY - floatFaceY / 30, floatFaceSize / 10, floatFaceSize / 10);
+  }
 }
+  
+
+    
+  
+
